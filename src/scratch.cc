c5a098db5da4c5e62eb44544154da78082eb4d5a
Christopher Haverman <phasetwenty@gmail.com>
2013-02-05 11:20:18 -0800

    Just some code-and-fix testing on how to make colors work. Might want it for later.

--- src/scratch.cc
+++ src/scratch.cc
@@ -1,14 +1,69 @@
+#include <cstdlib>
+#include <cstring>
 #include <ncurses.h>
 
-int main() {
-	initscr();			/* Start curses mode 		  */
-	getch();
+void print_in_middle(WINDOW *win,
+  int starty,
+  int startx,
+  int width,
+  char *string);
 
+int main(int argc, char *argv[]) {
+  initscr(); /* Start curses mode        */
+  noecho();
+  if (has_colors() == FALSE) {
+    endwin();
+    printf("Your terminal does not support color\n");
+    exit(1);
+  }
+  start_color(); /* Start color          */
+  init_pair(1, COLOR_RED, COLOR_BLACK);
+  init_pair(2, COLOR_WHITE, COLOR_RED);
 
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
+  print_in_middle(stdscr, LINES / 2, 0, 0, "Voila !!! In color ...");
 
-	return 0;
+  int ch = 0;
+  while ((ch = getch()) != 'q') {
+    switch (ch) {
+      case 'a': {
+        bkgd(COLOR_PAIR(1));
+        attroff(COLOR_PAIR(2));
+        attron(COLOR_PAIR(1));
+        refresh();
+        break;
+      }
+      case 'b': {
+        bkgd(COLOR_PAIR(2));
+        attroff(COLOR_PAIR(1));
+        attron(COLOR_PAIR(2));
+        refresh();
+        break;
+      }
+    }
+  }
+  endwin();
+}
+void print_in_middle(WINDOW *win,
+  int starty,
+  int startx,
+  int width,
+  char *string) {
+  int length, x, y;
+  float temp;
+
+  if (win == NULL)
+    win = stdscr;
+  getyx(win, y, x);
+  if (startx != 0)
+    x = startx;
+  if (starty != 0)
+    y = starty;
+  if (width == 0)
+    width = 80;
+
+  length = strlen(string);
+  temp = (width - length) / 2;
+  x = startx + (int) temp;
+  mvwprintw(win, y, x, "%s", string);
+  refresh();
 }

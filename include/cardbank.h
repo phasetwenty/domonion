9afc57b16cacf1a08734aa30c199239a6287760f
Christopher Haverman <phasetwenty@gmail.com>
2013-03-11 18:35:57 -0700

    More scaffolding on cards.

--- include/cardbank.h
+++ include/cardbank.h
@@ -9,19 +9,21 @@
 #define domonion_domonion_CARDBANK_H_
 
 #include <cards/base.h>
+#include <card.h>
 
-class Card;
-class std::vector<Card>;
+class std::vector<Card*>;
 
 namespace domonion {
 
 class CardBank {
 public:
   CardBank();
+  CardBank(std::vector<Card*> initial);
   virtual ~CardBank();
 
+
 private:
-  std::vector<Card> cards_;
+  std::vector<Card> all_cards_;
 
   CardBank(const CardBank& other) {}
 
--- include/cards/base.h
+++ include/cards/base.h
@@ -5,22 +5,45 @@
  *      Author: chris
  */
 
-#ifndef DOMONION_CARDS_LABORATORY_H_
-#define DOMONION_CARDS_LABORATORY_H_
+#ifndef DOMONION_CARDS_BASE_H_
+#define DOMONION_CARDS_BASE_H_
 
 #include <card.h>
 
 namespace domonion {
 namespace cards {
 
-class Laboratory: public Card {
+/*
+ * TODO
+ * * Card selection from the hand
+ * * Card reveal from the hand
+ * * Card reveal from the draw pile
+ */
+
+class Adventurer : public Card {
+public:
+  Adventurer();
+  virtual ~Adventurer();
+
+  virtual void Play(GameState& game) const;
+
+private:
+  Adventurer(const Adventurer& other);
+  Adventurer& operator=(const Adventurer& other);
+};
+
+class Laboratory : public Card {
 public:
   Laboratory();
   virtual ~Laboratory();
 
   virtual void Play(GameState& game) const;
+
+private:
+  Laboratory(const Laboratory& other);
+  Laboratory& operator=(const Laboratory& other);
 };
 
 } /* namespace cards */
 } /* namespace domonion */
-#endif /* DOMONION_CARDS_LABORATORY_H_ */
+#endif /* DOMONION_CARDS_BASE_H_ */
